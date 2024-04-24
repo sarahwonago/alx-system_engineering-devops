959fc4e35e4008a709a2c9e6c96b08eaf1e87d8e
Sarah Shaaleh Wonago <145486110+sarahwonago@users.noreply.github.com>
2024-04-24 18:07:35 +0300

    Update 7-puppet_install_nginx_web_server.pp

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,30 +1,25 @@
-# Update package repositories
+# Install NginX
+# With puppet
+
 exec { 'apt-get-update':
   command => '/usr/bin/apt-get update',
 }
 
-# Install Nginx package
 package { 'nginx':
   ensure  => installed,
   require => Exec['apt-get-update'],
 }
 
-# Create index.html file
 file { '/var/www/html/index.html':
   content => 'Hello World!',
   require => Package['nginx'],
 }
 
-# Configure redirection for /redirect_me
-file { '/etc/nginx/sites-available/default':
-  ensure  => present,
-  mode    => '0644',
-  content => template('nginx/default.conf.erb'), # Use a template file for better maintainability
-  require => Package['nginx'],
-  notify  => Service['nginx'], # Reload Nginx after configuration changes
+exec {'redirect_me':
+  command  => 'sed -i "24i\	rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default',
+  provider => 'shell'
 }
 
-# Ensure Nginx service is running
 service { 'nginx':
   ensure  => running,
   require => Package['nginx'],
